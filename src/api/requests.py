34ddb925760387283c94b3098f819ac44fbfd022
DESKTOP-BLUAJ2T\nogul <nogulo88@gmail.com>
2020-08-02 22:29:57 +0300

    Added jwt to Request creation  Resource

--- src/api/requests.py
+++ src/api/requests.py
@@ -1,5 +1,8 @@
-from flask import request, render_template
+from flask import request
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from libs.serving import response_quote
 from src.models.user import UserModel
 from src.models.requests import RequestModel
 
@@ -14,16 +17,19 @@ class RequestsList(Resource):
 class RequestCreation(Resource):
 
     @classmethod
+    @jwt_required
     def post(cls):
+        user = get_jwt_identity()
         data = request.get_json()
-        print(data)
+
+        if not UserModel.find_by_session_key(user):
+            return {"message": response_quote("code_401")}, 401
 
         user_request = RequestModel(
             theme=data["theme"],
             body=data["body"],
             status=data["status"]
         )
-
         user_request.save_to_db()
 
         return {"msg": "request created"}, 201
